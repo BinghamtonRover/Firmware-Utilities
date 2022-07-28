669ae7289777dc95feae547d246977373c465476
Levi Lesches <levilesches@gmail.com>
2022-07-28 05:19:37 -0400

    Finished documentation

--- src/BURT_can.h
+++ src/BURT_can.h
@@ -1,6 +1,7 @@
+/*! @file BURT_can.h */
+
 #ifndef burt_can_h
 #define burt_can_h
-
 #include <FlexCAN_T4.h>
 #include <functional>
 
@@ -9,12 +10,12 @@
 
 /// The CAN service.
 /// 
-/// For internal use only. 
+/// For internal use only. Uses the `FlexCAN_T4` library.
 typedef FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can;
 
-/// A message in the CAN protocol format.
-/// 
-/// See https://en.wikipedia.org/wiki/CAN_bus#Data_transmission for details.
+// A message in the CAN protocol format.
+// 
+// See https://en.wikipedia.org/wiki/CAN_bus#Data_transmission for details.
 typedef CAN_message_t CanMessage;
 
 /// A handler function that accepts a #CanMessage.
@@ -29,6 +30,8 @@ typedef void (*CanHandler)(const CanMessage& msg);
 /// receives a command from the operator, it translates it to a CAN message with a specific
 /// ID. The ID designates the purpose of the message as well as which function is to handle it.
 /// 
+/// Internally, this service delegates to the [`FlexCAN_T4`](https://github.com/tonton81/FlexCAN_T4) library.
+/// 
 /// Usage: 
 /// - Use #setup to initialize the CAN service
 /// - Use #update to check for new messages
@@ -39,7 +42,7 @@ typedef void (*CanHandler)(const CanMessage& msg);
 class BurtCan {
 	private: 
 		static int mailbox;  ///< The number of the next available mailbox.
-		static Can can;  ///< The FlexCAN_T4 instance.
+		static Can can;  ///< The `FlexCAN_T4` instance.
 
 	public: 
 		/// Initializes settings in CAN. 
@@ -61,7 +64,7 @@ class BurtCan {
 
 		/// Registers a handler function for the given ID.
 		/// 
-		/// When a message is received, the FlexCAN_T4 library will parse its ID and send it 
+		/// When a message is received, the `FlexCAN_T4` library will parse its ID and send it 
 		/// to a mailbox that accepts that ID. By calling this function, you create a mailbox
 		/// that is configured to handle all messages with the given ID and call the handler.
 		static void registerHandler(uint32_t id, CanHandler handler);
