8bd9019c5788dad66ca8be9853f2cd4e74206ef7
Seth Brokalis <71616577+sbrokal1@users.noreply.github.com>
2022-11-15 18:27:42 -0500

    Fix privacy for structs and flag packing

--- src/data/can/arm.h
+++ src/data/can/arm.h
@@ -1,57 +1,62 @@
 struct ArmPacket1 {
-    unsigned short swivelAngle;
-    unsigned short swivelTarget;
-    unsigned short extendAngle;
-    unsigned short extendTarget;
-    ArmPacket1(float swivelAngle, float swivelTarget, float extendAngle, float extendTarget) : 
-        swivelAngle((unsigned short) (swivelAngle*10000)),
-        swivelTarget((unsigned short) (swivelTarget*10000)),
-        extendAngle((unsigned short) (extendAngle*10000)),
-        extendTarget((unsigned short) (extendTarget*10000)) {}
-    float getSwivelAngle(){
-        return (float) swivelAngle/10000;
-    }
-    float getSwivelTarget(){
-        return (float) swivelTarget/10000;
-    }
-    float getExtendAngle(){
-        return (float) extendAngle/10000;
-    }
-    float getExtendTarget(){
-        return (float) extendTarget/10000;
-    }
+    private:
+        unsigned short swivelAngle;
+        unsigned short swivelTarget;
+        unsigned short extendAngle;
+        unsigned short extendTarget;
+    public:
+        ArmPacket1(float swivelAngle, float swivelTarget, float extendAngle, float extendTarget) : 
+            swivelAngle((unsigned short) (swivelAngle*10000)),
+            swivelTarget((unsigned short) (swivelTarget*10000)),
+            extendAngle((unsigned short) (extendAngle*10000)),
+            extendTarget((unsigned short) (extendTarget*10000)) {}
+        float getSwivelAngle(){
+            return (float) swivelAngle/10000;
+        }
+        float getSwivelTarget(){
+            return (float) swivelTarget/10000;
+        }
+        float getExtendAngle(){
+            return (float) extendAngle/10000;
+        }
+        float getExtendTarget(){
+            return (float) extendTarget/10000;
+        }
 
 };
 struct ArmPacket2 {
-    unsigned short liftAngle;
-    unsigned short liftTarget;
-    unsigned int flags : 6;
-    unsigned char swivelTemp;
-    unsigned char extendTemp;
-    unsigned char liftTemp;
-    ArmPacket2(float liftAngle, float liftTarget, bool swivelActivated, bool swivelLimSwitch, bool extendActivated, bool extendLimSwitch, bool liftActivated, bool liftLimSwitch, unsigned char swivelTemp, unsigned char extendTemp, unsigned char liftTemp ) : 
-        liftAngle((unsigned short) (liftAngle*10000)),
-        liftTarget((unsigned short) (liftTarget*10000)), flags(0), swivelTemp(swivelTemp), extendTemp(extendTemp), liftTemp(liftTemp) {
-            if(swivelActivated) flags|=0b1;
-            if(swivelLimSwitch) flags|=0b01;
-            if(extendActivated) flags|=0b001;
-            if(extendLimSwitch) flags|=0b0001;
-            if(liftActivated) flags|=0b00001;
-            if(liftLimSwitch) flags|=0b000001;
+    private:
+        unsigned short liftAngle;
+        unsigned short liftTarget;
+        bool swivelActivated : 1;
+        bool swivelLimSwitch : 1;
+        bool extendActivated : 1;
+        bool extendLimSwitch : 1;
+        bool liftActivated : 1;
+        bool liftLimSwitch : 1;
+        unsigned char swivelTemp;
+        unsigned char extendTemp;
+        unsigned char liftTemp;
+    public:
+        ArmPacket2(float liftAngle, float liftTarget, bool swivelActivated, bool swivelLimSwitch, bool extendActivated, bool extendLimSwitch, bool liftActivated, bool liftLimSwitch, unsigned char swivelTemp, unsigned char extendTemp, unsigned char liftTemp ) : 
+            liftAngle((unsigned short) (liftAngle*10000)),
+            liftTarget((unsigned short) (liftTarget*10000)), swivelTemp(swivelTemp), extendTemp(extendTemp), liftTemp(liftTemp),
+            swivelActivated(swivelActivated),swivelLimSwitch(swivelLimSwitch),extendActivated(extendActivated),
+            extendLimSwitch(extendLimSwitch),liftActivated(liftActivated),liftLimSwitch(liftLimSwitch)
+             {}
+        float getLiftAngle(){
+            return (float) liftAngle/10000;
         }
-    float getLiftAngle(){
-        return (float) liftAngle/10000;
-    }
-    float getLiftTarget(){
-        return (float) liftTarget/10000;
-    }
-    bool getSwivelActivated(){return (flags&0b1)>0;}
-    bool getSwivelLimSwitch(){return (flags&0b01)>0;}
-    bool getExtendActivated(){return (flags&0b001)>0;}
-    bool getExtendLimSwitch(){return (flags&0b0001)>0;}
-    bool getLiftActivated(){return (flags&0b00001)>0;}
-    bool getLiftLimSwitch(){return (flags&0b000001)>0;}
-    unsigned char getSwivelTemp(){return swivelTemp;}
-    unsigned char getExtendTemp(){return extendTemp;}
-    unsigned char getLiftTemp(){return liftTemp;}
+        float getLiftTarget(){
+            return (float) liftTarget/10000;
+        }
+        bool getSwivelActivated(){return swivelActivated;}
+        bool getSwivelLimSwitch(){return swivelLimSwitch;}
+        bool getExtendActivated(){return extendActivated;}
+        bool getExtendLimSwitch(){return extendLimSwitch;}
+        bool getLiftActivated(){return liftActivated;}
+        bool getLiftLimSwitch(){return liftLimSwitch;}
+        unsigned char getSwivelTemp(){return swivelTemp;}
+        unsigned char getExtendTemp(){return extendTemp;}
+        unsigned char getLiftTemp(){return liftTemp;}
 };
--- src/data/can/gripper.h
+++ src/data/can/gripper.h
@@ -1,57 +1,63 @@
+
 struct GripperPacket1 {
-    unsigned short rotateAngle;
-    unsigned short rotateTarget;
-    unsigned short pinchAngle;
-    unsigned short pinchTarget;
-    GripperPacket1(float rotateAngle, float rotateTarget, float pinchAngle, float pinchTarget) : 
-        rotateAngle((unsigned short) (rotateAngle*10000)),
-        rotateTarget((unsigned short) (rotateTarget*10000)),
-        pinchAngle((unsigned short) (pinchAngle*10000)),
-        pinchTarget((unsigned short) (pinchTarget*10000)) {}
-    float getRotateAngle(){
-        return (float) rotateAngle/10000;
-    }
-    float getRotateTarget(){
-        return (float) rotateTarget/10000;
-    }
-    float getPinchAngle(){
-        return (float) pinchAngle/10000;
-    }
-    float getPinchTarget(){
-        return (float) pinchTarget/10000;
-    }
+    private:
+        unsigned short rotateAngle;
+        unsigned short rotateTarget;
+        unsigned short pinchAngle;
+        unsigned short pinchTarget;
+    public:
+        GripperPacket1(float rotateAngle, float rotateTarget, float pinchAngle, float pinchTarget) : 
+            rotateAngle((unsigned short) (rotateAngle*10000)),
+            rotateTarget((unsigned short) (rotateTarget*10000)),
+            pinchAngle((unsigned short) (pinchAngle*10000)),
+            pinchTarget((unsigned short) (pinchTarget*10000)) {}
+        float getRotateAngle(){
+            return (float) rotateAngle/10000;
+        }
+        float getRotateTarget(){
+            return (float) rotateTarget/10000;
+        }
+        float getPinchAngle(){
+            return (float) pinchAngle/10000;
+        }
+        float getPinchTarget(){
+            return (float) pinchTarget/10000;
+        }
 
 };
 struct GripperPacket2 {
-    unsigned short liftAngle;
-    unsigned short liftTarget;
-    unsigned int flags : 6;
-    unsigned char rotateTemp;
-    unsigned char pinchTemp;
-    unsigned char liftTemp;
-    GripperPacket2(float liftAngle, float liftTarget, bool rotateActivated, bool rotateLimSwitch, bool pinchActivated, bool pinchLimSwitch, bool liftActivated, bool liftLimSwitch, unsigned char rotateTemp, unsigned char pinchTemp, unsigned char liftTemp ) : 
-        liftAngle((unsigned short) (liftAngle*10000)),
-        liftTarget((unsigned short) (liftTarget*10000)), flags(0), rotateTemp(rotateTemp), pinchTemp(pinchTemp), liftTemp(liftTemp) {
-            if(rotateActivated) flags|=0b1;
-            if(rotateLimSwitch) flags|=0b01;
-            if(pinchActivated) flags|=0b001;
-            if(pinchLimSwitch) flags|=0b0001;
-            if(liftActivated) flags|=0b00001;
-            if(liftLimSwitch) flags|=0b000001;
+    private:
+        unsigned short liftAngle;
+        unsigned short liftTarget;
+        bool rotateActivated : 1;
+        bool rotateLimSwitch : 1;
+        bool pinchActivated : 1;
+        bool pinchLimSwitch : 1;
+        bool liftActivated : 1;
+        bool liftLimSwitch : 1;
+        unsigned char rotateTemp;
+        unsigned char pinchTemp;
+        unsigned char liftTemp;
+    public:
+        GripperPacket2(float liftAngle, float liftTarget, bool rotateActivated, bool rotateLimSwitch, bool pinchActivated, bool pinchLimSwitch, bool liftActivated, bool liftLimSwitch, unsigned char rotateTemp, unsigned char pinchTemp, unsigned char liftTemp ) : 
+            liftAngle((unsigned short) (liftAngle*10000)),
+            liftTarget((unsigned short) (liftTarget*10000)), rotateTemp(rotateTemp), pinchTemp(pinchTemp), liftTemp(liftTemp),
+            rotateActivated(rotateActivated),rotateLimSwitch(rotateActivated),pinchActivated(pinchActivated),
+            pinchLimSwitch(pinchLimSwitch),liftActivated(liftActivated),liftLimSwitch(liftLimSwitch) {
+            }
+        float getLiftAngle(){
+            return (float) liftAngle/10000;
         }
-    float getLiftAngle(){
-        return (float) liftAngle/10000;
-    }
-    float getLiftTarget(){
-        return (float) liftTarget/10000;
-    }
-    bool getRotateActivated(){return (flags&0b1)>0;}
-    bool getRotateLimSwitch(){return (flags&0b01)>0;}
-    bool getPinchActivated(){return (flags&0b001)>0;}
-    bool getPinchLimSwitch(){return (flags&0b0001)>0;}
-    bool getLiftActivated(){return (flags&0b00001)>0;}
-    bool getLiftLimSwitch(){return (flags&0b000001)>0;}
-    unsigned char getRotateTemp(){return rotateTemp;}
-    unsigned char getPinchTemp(){return pinchTemp;}
-    unsigned char getLiftTemp(){return liftTemp;}
+        float getLiftTarget(){
+            return (float) liftTarget/10000;
+        }
+        bool getRotateActivated(){return rotateActivated;}
+        bool getRotateLimSwitch(){return rotateLimSwitch;}
+        bool getPinchActivated(){return pinchActivated;}
+        bool getPinchLimSwitch(){return pinchLimSwitch;}
+        bool getLiftActivated(){return liftActivated;}
+        bool getLiftLimSwitch(){return liftLimSwitch;}
+        unsigned char getRotateTemp(){return rotateTemp;}
+        unsigned char getPinchTemp(){return pinchTemp;}
+        unsigned char getLiftTemp(){return liftTemp;}
 };
