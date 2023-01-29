6d77d57bd74262bb5c7745aadcb060a61d330977
Levi Lesches <levilesches@gmail.com>
2023-01-29 15:46:23 -0500

    Added new sync batch/bash scripts

---
+++ sync.bat
@@ -0,0 +1,35 @@
+@echo off
+
+rem Run this script on Windows to sync the libraries in this repo to your Arduino sketchbook. 
+
+rem This allows variables to be evaluated after declared.
+rem Why this is not the default baffles me.
+setlocal ENABLEDELAYEDEXPANSION
+
+rem Parse user's sketchbook as the first argument
+if [%1] == [] (
+	set sketchbook=%userprofile%\Documents\Arduino\libraries
+	echo Sketchbook not provided. Using Windows default of !sketchbook!
+	echo To override this, pass your sketchbook's libraries folder as the first argument.
+) else (
+	echo Using user-provided sketchbook location: %1
+	set sketchbook=%1
+)
+
+rem Verify sketchbook directory exists
+if NOT exist !sketchbook! (
+	echo "Error: Cannot find sketchbook directory: !sketchbook!"
+	pause
+	exit /b 1
+)
+
+rem Sync all libraries to the sketchbook
+echo.
+if exist !sketchbook!\BURT_can (
+	echo Folder already exists
+) else (
+	mklink /j !sketchbook!\BURT_can .
+	if ERRORLEVEL 1 (echo Operation failed) else (echo Done)
+) 
+
+pause 
