7d08476911fa4fed8accb97caaac682a99b94007
Levi Lesches <levilesches@gmail.com>
2023-01-29 15:44:19 -0500

    Incorporated nanopb

---
+++ src/BURT_proto.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "nanopb/pb.h"
+#include "nanopb/pb_common.h"
+#include "nanopb/pb_encode.h"
+#include "nanopb/pb_decode.h"
+
+class BurtProto {
+	public:
+		static int encode(uint8_t* buffer, const pb_msgdesc_t* fields, const void* message);
+		static bool decodeRaw(const uint8_t* buffer, const pb_msgdesc_t* fields, void* message);
+
+		template<typename T>
+		static T decode(const uint8_t* buffer, const pb_msgdesc_t* fields) {
+			T result;
+			decodeRaw(buffer, fields, &result);
+			return result;
+		}
+};
