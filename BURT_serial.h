90bbaf8094401dea0c651c8f0cdfeac213791371
Levi Lesches <levilesches@gmail.com>
2024-01-30 15:33:09 -0500

    Gave BurtSerial a blank setup

--- BURT_serial.h
+++ BURT_serial.h
@@ -9,6 +9,7 @@ class BurtSerial {
 		bool isConnected = false;
 
 		BurtSerial(Device device, ProtoHandler onMessage, VoidCallback onDisconnect);
+		void setup() { /* No setup needed */ }
 		void update();
 		bool send(const pb_msgdesc_t* fields, const void* message, int length);
 
