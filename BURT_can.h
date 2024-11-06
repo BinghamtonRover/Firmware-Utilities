b1d523f142b38c5023677dc199d888e9b62e2b84
Ruari Randall <165113847+ruari-r@users.noreply.github.com>
2024-11-06 09:02:19 -0500

    Change Mailbox type to allow for extended read (#21)

--- BURT_can.h
+++ BURT_can.h
@@ -13,56 +13,66 @@
 /// The CAN service using the Teensy pins at CAN1.
 using Can1 = FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>;
 
-/// The CAN service using the Teensy pins at CAN2. 
+/// The CAN service using the Teensy pins at CAN2.
 using Can2 = FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16>;
 
-/// The CAN service using the Teensy pins at CAN3. 
+/// The CAN service using the Teensy pins at CAN3.
 using Can3 = FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16>;
 
 // A message in the CAN protocol format.
-// 
+//
 // See https://en.wikipedia.org/wiki/CAN_bus#Data_transmission for details.
 using CanMessage = CAN_message_t;
 
 
 /// A service to send and receive messages via the CAN bus protocol.
-/// 
-/// The CAN protocol allows microcontrollers to efficiently communicate. On the rover, 
+///
+/// The CAN protocol allows microcontrollers to efficiently communicate. On the rover,
 /// CAN is used to coordinate the subsystems to the onboard computer. When the computer
 /// receives a command from the operator, it translates it to a CAN message with a specific
 /// ID. The ID designates the purpose of the message as well as which function is to handle it.
-/// 
+///
 /// Internally, this service delegates to the [`FlexCAN_T4`](https://github.com/tonton81/FlexCAN_T4) library.
-/// 
-/// Usage: 
+///
+/// Usage:
 /// - Create an instance and pass in the ID and callback to handle incoming messages
 /// - Use #setup to initialize the CAN service
 /// - Use #update to check for new messages
 /// - Use #send to send a message with a given ID
-/// 
+///
 /// See https://en.wikipedia.org/wiki/CAN_bus for more details.
 template <class CanType>
 class BurtCan {
-	private: 
+	private:
 		/// The underlying `FlexCAN_T4` instance.
 		CanType can;
 
-		/// The ID of the message this instance will respond to.
-		uint32_t id;
+		/// The start of the range of IDs this mailbox will listen to.
+		uint32_t idStart;
 
+		/// The end of the range of IDs this mailbox will listen to.
+		///
+		/// A value of zero indicates no range, just #idStart.
+		uint32_t idEnd;
+
+		/// A user-provided callback to run when a message is received.
 		ProtoHandler onMessage;
 
 		bool useExtendedIds;
 
+		/// Calls #onMessage when new messages are received.
 		void handleCanFrame(const CanMessage& message);
 
-	public: 
-		/// Creates a CAN handler that calls #onMessage for messages with the given ID.
+	public:
+		/// A CAN mailbox that listens to messages with one CAN ID.
 		BurtCan(uint32_t id, ProtoHandler onMessage, bool useExtendedIds = false);
 
-		/// Initializes the CAN hardware to handle messages with #id.
-		/// 
-		/// When a message is received, the `FlexCAN_T4` library will parse its ID and send it 
+		/// A CAN mailbox that listens to messages with a range of CAN IDs.
+		BurtCan(uint32_t idStart, uint32_t idEnd, ProtoHandler onMessage, bool useExtendedIds = false);
+
+		/// Initializes the CAN hardware to handle messages with the given ID(s).
+		///
+		/// When a message is received, the `FlexCAN_T4` library will parse its ID and send it
 		/// to a mailbox that accepts that ID. By calling this function, you create a mailbox
 		/// that is configured to handle all messages with the given ID and call the handler.
 		void setup();
