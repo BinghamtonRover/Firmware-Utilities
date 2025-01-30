b5d503cd8dcbd17ff6d677a2453a372fac290e53
Gold87 <91761103+Gold872@users.noreply.github.com>
2025-01-30 15:09:10 -0500

    Add debug print method

--- BURT_utils.h
+++ BURT_utils.h
@@ -4,3 +4,19 @@
 #include "BURT_proto.h"
 #include "BURT_serial.h"
 #include "BURT_timer.h"
+
+// Utility methods for printing only when debug mode is defined,
+// this will prevent any text from being written to Serial when
+// running on the Rover
+//
+// In order to have messages print to Serial, add at the top of your .ino file:
+// #define DEBUG_MODE
+#ifdef DEBUG_MODE
+#define DebugPrint(message) Serial.print(message)
+#define DebugPrintln(message) Serial.println(message)
+#define DebugPrintHEX(message) Serial.print(message, HEX)
+#else
+#define DebugPrint(message)
+#define DebugPrintln(message)
+#define DebugPrintHEX(message)
+#endif
