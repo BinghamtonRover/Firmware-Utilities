868c4cf659ab394c78f830719512f046c135390b
ruari-r <ruarirandall@icloud.com>
2026-01-29 17:37:10 -0500

    Add extra comment describing use

--- BURT_spi.h
+++ BURT_spi.h
@@ -3,6 +3,18 @@
 #include <SPI.h>
 #include <stdint.h>
 
+/* 
+Call Order:
+if (BurtSPI.prepareTransaction(device_addr)) {
+    device.func_that_uses_SPI(out_EN);
+}
+BurtSPI.goToIdle();
+
+This will ensure that the bus is idle, then set up the correct virtual CS/addr pin
+then call the external library's function with the out_EN pin so it can handle timing
+then close the transaction and go back to idle
+*/
+
 /// @brief For use with 74hc4515 4:16 Decoder IC for Chip Select
 /// @param CS_addr array of 4 pins serving as addressing pins to the decoder
 /// @param addr_EN enables the decoder to change its addr
