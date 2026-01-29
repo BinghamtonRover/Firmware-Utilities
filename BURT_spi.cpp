60c8d2300e665b8d95e99c9d73aa8a05f3886e22
ruari-r <ruarirandall@icloud.com>
2026-01-29 16:34:19 -0500

    Implement setup method, add delay field to ctor

--- BURT_spi.cpp
+++ BURT_spi.cpp
@@ -1,14 +1,22 @@
 #include "BURT_spi.h"
 
-BurtSPI::BurtSPI(uint8_t CS_addr[4], uint8_t addr_EN, uint8_t out_EN) :
+
+BurtSPI::BurtSPI(uint8_t CS_addr[4], uint8_t addr_EN, uint8_t out_EN, unsigned delay_us) :
     CS_addr(CS_addr),
     addr_EN(addr_EN),
-    out_EN(out_EN)
+    out_EN(out_EN),
+    delay_us(delay_us)
     { }
 
-BurtSPI::prepareTransaction(uint8_t addr) {
-    if (!ready) {
-        Serial.println("SPI Bus not ready for transaction");
-        return false;
-    }
+BurtSPI::setupSPI() {
+    pinMode(CS_addr[0], OUTPUT);
+    pinMode(CS_addr[1], OUTPUT);
+    pinMode(CS_addr[2], OUTPUT);
+    pinMode(CS_addr[3], OUTPUT);
+    pinMode(addr_EN,    OUTPUT);
+    pinMode(out_EN,     OUTPUT);
+
+    SPI.begin();
+    Serial.println("SPI Bus Initialized");
 }
+
--- BURT_spi.h
+++ BURT_spi.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Arduino.h>
+#include <SPI.h>
 
 class BurtSPI {
     private:
@@ -9,7 +10,7 @@ class BurtSPI {
 
         bool ready = false;
     public:
-        BurtSPI(uint8_t CS_addr[4], uint8_t addr_EN, uint8_t out_EN);
+        BurtSPI(uint8_t CS_addr[4], uint8_t addr_EN, uint8_t out_EN, unsigned delay_us);
 
         void setupSPI();
         bool prepareTransaction(uint8_t addr);
