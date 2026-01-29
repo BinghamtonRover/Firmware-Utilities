258393743aa6f196df44b7eef8c8ebc6c1d24d7f
ruari-r <ruarirandall@icloud.com>
2026-01-29 17:12:31 -0500

    Add brief to .h

--- BURT_spi.h
+++ BURT_spi.h
@@ -2,6 +2,11 @@
 #include <Arduino.h>
 #include <SPI.h>
 
+/// @brief For use with 74hc4515 4:16 Decoder IC for Chip Select
+/// @param CS_addr array of 4 pins serving as addressing pins to the decoder
+/// @param addr_EN enables the decoder to change its addr
+/// @param out_EN enables the selected output (active LOW)
+/// @param delay_us optional; small delay after successful transaction (keep below 10us)
 class BurtSPI {
     private:
         uint8_t CS_addr[4];
